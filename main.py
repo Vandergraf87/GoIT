81d8d76891e66eef0a6633437355684c72cd7d1e
Vandergraf87 <komarov.dmytro@gmail.com>
2023-07-11 01:30:08 +0300

    frew

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-dsa = 123
-#update1
-#uipodate2
+work_experience = int(input("Enter your full work experience in years: "))
+
+if work_experience <= 1:
+    developer_type = "Junior"
+elif work_experience > 5:
+    developer_type = "Senior"
+else:
+    developer_type = "Middle"
+
+print(f"{developer_type}")
