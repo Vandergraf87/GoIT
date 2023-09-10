0e47ba499c1bf6ffa7e95ee7649d34acda5c29c0
Vandergraf87 <komarov.dmytro@gmail.com>
2023-09-10 23:34:34 +0300

    looks like ready

--- HW_4/main.py
+++ HW_4/main.py
@@ -1,6 +1,19 @@
 
 contact_list = dict()
 
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "Enter user name"
+        except ValueError:
+            return "Give me name and phone please"
+        except IndexError:
+            return "Invalid command"
+    return inner
+
+@input_error
 def main():
     while True:
         user_input = input('Please enter something: ')
@@ -9,35 +22,51 @@ def main():
         elif user_input.lower().find('good bye') != -1 or user_input.lower().find('close') != -1 or user_input.lower().find('exit') != -1:
             print('Good bye!')
             break
+        elif user_input.lower().find('add') != -1 or user_input.lower().find('change') != -1 or\
+             user_input.lower().find('phone') != -1 or user_input.lower().find('show all') != -1:
+            if command_handler(user_input) is None:
+                pass 
+            else:
+                print(command_handler(user_input))
         else:
-            print(contact_list)
-            command_parser(user_input)
+            print("Invalid command")
 
-def command_parser(input_command):
+
+@input_error
+def command_handler(input_command):
     if input_command.lower().find('add') != -1:
-        add_contact(input_command)
+        return add_contact(input_command)
     elif input_command.lower().find('change') != -1:
-        change_contact(input_command)
+        return change_contact(input_command)
     elif input_command.lower().find('phone') != -1:
-        show_phone_number(input_command)
+        return show_phone_number(input_command)
     elif input_command.lower().find('show all') != -1:
-        show_all_contact(input_command)
+        return show_all_contact(input_command)
 
+@input_error
 def add_contact(input_text):
     convert_2_list = input_text.split()
-    print(convert_2_list)
     contact_list.update({convert_2_list[1]: convert_2_list[2]})
 
+@input_error
 def change_contact(input_text):
     convert_2_list = input_text.split()
     contact_list[convert_2_list[1]] = convert_2_list[2]
 
+@input_error
 def show_phone_number(input_text):
     convert_2_list = input_text.split()
-    print(contact_list[convert_2_list[1]])
+    if convert_2_list[1] in contact_list:
+        return contact_list[convert_2_list[1]]
+    else:
+        return 'Not found'
 
+@input_error
 def show_all_contact(input_text):
-    for name, phone in contact_list.items():
-        print(f'{name}: {phone}')
+    if not contact_list:
+        return 'No records found'
+    else:
+        return "\n".join([f"{name.title()}: {phone}" for name, phone in contact_list.items()])
 
-main()
+if __name__ == "__main__":
+    main()
