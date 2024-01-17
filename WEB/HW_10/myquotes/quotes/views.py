7d78bbb885fb8eb449d92b7284c52b5ea951c495
Vandergraf87 <komarov.dmytro@gmail.com>
2024-01-17 21:57:01 +0200

    hw10 start

---
+++ WEB/HW_10/myquotes/quotes/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from .forms import AuthorForm, QuoteForm
+from .models import Author, Quote
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def add_author(request):
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('author_list')
+    else:
+        form = AuthorForm()
+    return render(request, 'myquotes/add_author.html', {'form': form})
+
+@login_required
+def add_quote(request):
+    if request.method == 'POST':
+        form = QuoteForm(request.POST)
+        if form.is_valid():
+            quote = form.save(commit=False)
+            quote.user = request.user
+            quote.save()
+            return redirect('quote_list')
+    else:
+        form = QuoteForm()
+    return render(request, 'myquotes/add_quote.html', {'form': form})
+
